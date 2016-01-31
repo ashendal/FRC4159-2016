fbfde9d38188388a558c1bdb10fe07cf0cb9d10b
Cole Scott <colescottsf@gmail.com>
2016-01-30 19:50:11 -0800

    Remove default crap

--- src/org/usfirst/frc/team4159/robot/Robot.java
+++ src/org/usfirst/frc/team4159/robot/Robot.java
@@ -5,102 +5,99 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import org.usfirst.frc.team4159.robot.commands.ExampleCommand;
-import org.usfirst.frc.team4159.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
+ * Main robot class
  */
 public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
 
     Command autonomousCommand;
     SendableChooser chooser;
 
     /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
+     * Main robot initialization method
      */
     public void robotInit() {
 		oi = new OI();
         chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", new ExampleCommand());
+        chooser.addDefault("Default Auto", null);
 //        chooser.addObject("My Auto", new MyAutoCommand());
         SmartDashboard.putData("Auto mode", chooser);
     }
 	
 	/**
-     * This function is called once each time the robot enters Disabled mode.
-     * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
+     * Robot disable method
+     * 
+     * Used to reset subsystems and values
      */
     public void disabledInit(){
 
     }
 	
+    /**
+	 * Disabled periodic function
+	 * 
+	 * Nothing should run here...
+	 */
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
 	}
 
 	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the getString code to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional commands to the chooser code above (like the commented example)
-	 * or additional comparisons to the switch structure below with additional strings & commands.
+	 * Autonomous initialization function
+	 * 
+	 * Read autonomous mode from SmartDashboard and run that command
 	 */
     public void autonomousInit() {
         autonomousCommand = (Command) chooser.getSelected();
-        
-		/* String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
-		switch(autoSelected) {
-		case "My Auto":
-			autonomousCommand = new MyAutoCommand();
-			break;
-		case "Default Auto":
-		default:
-			autonomousCommand = new ExampleCommand();
-			break;
-		} */
     	
     	// schedule the autonomous command (example)
         if (autonomousCommand != null) autonomousCommand.start();
     }
 
     /**
-     * This function is called periodically during autonomous
+     * Autonomous periodic method
      */
     public void autonomousPeriodic() {
         Scheduler.getInstance().run();
     }
-
+    
+    /**
+	 * Teleop initialization function
+	 * 
+	 * Kill autonomous command and setup any subsystems for teleop mode
+	 */
     public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to 
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
+    	// kill autonomous command at start of teleop
         if (autonomousCommand != null) autonomousCommand.cancel();
     }
 
     /**
-     * This function is called periodically during operator control
+     * Teleop periodic method
+     * 
+     * Get and act on operator input and run commands
      */
     public void teleopPeriodic() {
         Scheduler.getInstance().run();
     }
     
     /**
-     * This function is called periodically during test mode
+     * Test initialization method
+     * 
+     * Setup subsystems and SmartDashboard for testing
+     */
+    public void testInit() {
+        
+    }
+    
+    /**
+     * Test periodic method
+     * 
+     * Teleop periodic plus writing to SmartDashboard
      */
     public void testPeriodic() {
         LiveWindow.run();
