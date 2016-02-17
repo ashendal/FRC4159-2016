a51c522bb6057182034011632ca8cb23acf5102b
Cole Scott <colescottsf@gmail.com>
2016-02-17 10:44:04 -0800

    Add comments

--- src/org/usfirst/frc/team4159/robot/subsystems/Lifter.java
+++ src/org/usfirst/frc/team4159/robot/subsystems/Lifter.java
@@ -10,18 +10,25 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- *
+ * Lifter subsystem
+ * 
+ * @author Cole Scott
  */
 public class Lifter extends Subsystem {
     
     private static double JOYSTICK_MULT = 5.0;
     
-    Victor lifterDart;
+    private Victor lifterDart;
     
-    Encoder lifterEncoder;
+    private Encoder lifterEncoder;
     
-    PIDController lifterPID;
+    private PIDController lifterPID;
     
+    /**
+     * Main Lifter constructor<br>
+     * <br>
+     * Setup all motors, encoders, and PID<br>
+     */
     public Lifter()
     {
         lifterDart = new Victor(RobotMap.dartActuator);
@@ -29,26 +36,47 @@ public class Lifter extends Subsystem {
         lifterPID = new PIDController(SmartDashboard.getNumber("Lifter.lifterPID.kP"), SmartDashboard.getNumber("Lifter.lifterPID.kI"), SmartDashboard.getNumber("Lifter.lifterPID.kD"), lifterEncoder, lifterDart);
     }
     
+    /**
+     * Enable subsystem
+     */
     public void enable()
     {
         lifterPID.enable();
     }
     
+    /**
+     * Disable subsystem
+     */
     public void disable()
     {
         lifterPID.disable();
     }
     
+    /**
+     * Feed joystick value to use to move lifter
+     * 
+     * @param joystick Joystick for input
+     */
     public void feedJoystick(Joystick joystick)
     {
         setAngle(getAngle() + (joystick.getY() * JOYSTICK_MULT));
     }
     
+    /**
+     * Set angle of lifter
+     * 
+     * @param angle Angle setpoint
+     */
     public void setAngle(double angle)
     {
         lifterPID.setSetpoint(getValueFromAngle(angle));
     }
     
+    /**
+     * Get current angle of lifter
+     * 
+     * @return Angle
+     */
     public double getAngle()
     {
         return getAngleFromValue(lifterEncoder.get());
@@ -57,11 +85,27 @@ public class Lifter extends Subsystem {
     public void initDefaultCommand() {
     }
     
+    /**
+     * Get encoder value from given angle<br>
+     * <br>
+     * Used for setting PID 
+     * 
+     * @param degrees Degrees input
+     * @return Encoder value output
+     */
     private double getValueFromAngle(double degrees)
     {
         return 2965 + (-11.33 * -1 *degrees);
     }
     
+    /**
+     * Get angle given encoder value<br>
+     * <br>
+     * Used for returning angle of lifter 
+     * 
+     * @param value Encoder value
+     * @return Angle of lifter
+     */
     private double getAngleFromValue(double value)
     {
         return -1*(261.6 + (-0.08824 * value));
