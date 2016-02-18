1c7a95d15a8c205f4b96d85c2055ed462a1c31bf
Sushikat <ikatbuschel@gmail.com>
2016-02-17 20:02:11 -0800

    Fixed shooter lookup class to use a linear function instead of a lookup table

--- src/org/usfirst/frc/team4159/robot/ShooterLookup.java
+++ src/org/usfirst/frc/team4159/robot/ShooterLookup.java
@@ -6,57 +6,19 @@ import java.util.ArrayList;
  * 
  * @author Iris
  */
-public class ShooterLookup {
-<<<<<<< HEAD
-=======
-    // ArrayList contains array pairs of {Distance, Angle}
-    // CURRENTLY CONTAINS RANDOM VALUES! DELETE THIS WHEN ARRAYLIST IS POPUlATED
-    static ArrayList<Pair> lookupTable = new ArrayList<Pair>();
-    public ShooterLookup(){
-        for(int i = 0; i < 90; i += 5)
-            lookupTable.add(new Pair(i, (i+30)));
-    }
-    
->>>>>>> c08d44ee8db0001209792633ea59fea1c154c264
+public class ShooterLookup {    
     /**
      * Useless constructor that exists for the sake of having a constructor
      */
-<<<<<<< HEAD
     public ShooterLookup(){};
-=======
-    private int getRoundDist(double x){
-        int out = (int)x;
-        if( out%10 < 5 ){
-            if(out%5 < 3)
-                out = out - (out%5);
-            else
-                out = out + (5 - out%5);
-        }else{
-            if(out%5 < 8)
-                out = out - (out%5);
-            else
-                out = out + (5 - out%5);
-        }
-        return out;
-    }
->>>>>>> c08d44ee8db0001209792633ea59fea1c154c264
     
     /**
-     * Looks up angle in lookup table
-     * @param dist the unrounded camera distance
-     * @return the angle required to shoot into the high goal or -1 if value not found
+     * Finds an angle given a distance
+     * 
+     * @param dist the distance, in inches, provided by the camera
+     * @return the angle to shoot at
      */
-<<<<<<< HEAD
     public double getAngle(double dist){
         return 1.862*(dist/12.0) + 34.79;
-=======
-    public int getAngle(double dist){
-        int rDist = getRoundDist(dist);
-        for(int i = 0; i < lookupTable.size(); i++){
-             if(lookupTable.get(i).distance == rDist)
-                 return lookupTable.get(i).angle;
-        }
-        return -1;
->>>>>>> c08d44ee8db0001209792633ea59fea1c154c264
     }
 }
