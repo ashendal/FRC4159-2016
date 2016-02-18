1d69d3216245d71c19e8f71f225af1c4d2fce3a5
Cole Scott <colescottsf@gmail.com>
2016-02-18 11:03:18 -0800

    Update RobotMap to real values

--- src/org/usfirst/frc/team4159/robot/RobotMap.java
+++ src/org/usfirst/frc/team4159/robot/RobotMap.java
@@ -7,41 +7,38 @@ package org.usfirst.frc.team4159.robot;
  * floating around.
  */
 public class RobotMap {
-    
-    //TODO: Set port values based on real robot wiring
-    
+        
     // Drivetrain PWM ports
     public static int leftDriveMotor = 0;
-    public static int rightDriveMotor = 0;
+    public static int rightDriveMotor = 4;
     
     // Drivetrain encoder ports
     public static int leftDriveEncoderA = 0;
-    public static int leftDriveEncoderB = 0;
-    public static int rightDriveEncoderA = 0;
-    public static int rightDriveEncoderB = 0;
+    public static int leftDriveEncoderB = 1;
+    public static int rightDriveEncoderA = 2;
+    public static int rightDriveEncoderB = 3;
     
     // Drivetrain solenoid ports
-    public static int leftShiftPistonForwards = 0;
-    public static int leftShiftPistonReverse = 0;
-    public static int rightShiftPistonForwards = 0;
-    public static int rightShiftPistonReverse = 0;
+    public static int leftShiftPistonForwards = 5;
+    public static int leftShiftPistonReverse = 1;
+    public static int rightShiftPistonForwards = 6;
+    public static int rightShiftPistonReverse = 2;
     
     // Shooter PWM ports
-    public static int topShooter = 0;
-    public static int bottomShooter = 0;
+    public static int topShooter = 2;
+    public static int bottomShooter = 6;
     
     // Shooter encoder ports
-    public static int topShooterEncoderA = 0;
-    public static int topShooterEncoderB = 0;
-    public static int bottomShooterEncoderA = 0;
-    public static int bottomShooterEncoderB = 0;
+    public static int topShooterEncoderA = 4;
+    public static int topShooterEncoderB = 5;
+    public static int bottomShooterEncoderA = 6;
+    public static int bottomShooterEncoderB = 7;
     
     // Shooter solenoid ports
     public static int shooterTriggerForwards = 0;
-    public static int shooterTriggerReverse = 0;
+    public static int shooterTriggerReverse = 4;
     
     //lifter PWM ports
-    public static int dartActuator = 0;
-    public static int dartEncoderA = 0;
-    public static int dartEncoderB = 0;
+    public static int dartActuator = 3;
+    public static int dartEncoder = 0;
 }
